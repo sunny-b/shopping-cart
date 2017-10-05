39fd2bd3b847295e4851ce25f4d0a494a4507c04
Sunny <sjbeatteay@gmail.com>
2017-10-05 13:09:56 -0400

    wip

--- src/App.js
+++ src/App.js
@@ -2,20 +2,58 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class ShoppingCart extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1>Shopping Cart Example</h1>
+        <hr />
+        <ProductList />
+        <Cart />
       </div>
     );
   }
 }
 
-export default App;
+class ProductList extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Products</h2>
+        <ul>
+            <Product description='iPad Mini' price={500.01} amount={3} />
+            <Product description='T-Shirt' price={10.01} amount={5}/>
+        </ul>
+        <hr />
+      </div>
+    );
+  }
+}
+
+class Product extends Component {
+  render() {
+    return (
+      <li>
+        <p>
+          <span>{this.props.description}</span> -
+          <span>{this.props.price}</span> x
+          <span>{this.props.amount}</span>
+        </p>
+        <button>Add to cart</button>
+      </li>
+    );
+  }
+}
+
+class Cart extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Your Cart</h2>
+
+      </div>
+    );
+  }
+}
+
+export default ShoppingCart;
